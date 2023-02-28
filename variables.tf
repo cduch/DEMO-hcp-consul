413108d28d705b4120ddca881c5565c6d1c854a1
Carsten Duch <carsten@hashicorp.com>
2023-02-28 11:48:35 +0100

    added declaration for HCP credentials

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "hcp_client_id" {
+  description = "The HCP Client ID"
+  type        = string
+  sensitive   = false
+}
+
+variable "hcp_client_secret" {
+  description = "The HCP Client Secret (sensitive)"
+  type        = string
+  sensitive   = true
+}
+
 
 variable "tier" {
   type = string

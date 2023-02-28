fedbc37552834c1396b72506101722ac6f428a02
Carsten Duch <carsten@hashicorp.com>
2023-02-28 11:27:25 +0100

    added consul cluster

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "hcp" {
+  client_id     = var.hcp_client_id
+  client_secret = var.hcp_client_secret
+}
+
+
+data "terraform_remote_state" "hvn" {
+  backend = "remote"
+
+  config = {
+    organization = var.organization
+    workspaces = {
+      name = var.hvnworkspace
+    }
+  }
+}
+
+
+resource "hcp_consul_cluster" "consulcluster" {
+  cluster_id = "consul-cluster"
+  hvn_id     = data.terraform_remote_state.hvn.output.hvn_id
+  tier       = var.tier
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "tier" {
+  type = string
+  default = "standard"
+}
+
+variable "organization" {
+    type = string
+    default = "carstenduch"
+}
+variable "hvnworkspace" {
+  type = string
+  default = "HCP-HashiCorp_Virtual_Network-AWS_eu-central-1"
+}

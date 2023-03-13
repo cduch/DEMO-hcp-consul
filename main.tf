4baecf66facaa6e06876c9eec501ebdaff274720
Carsten Duch <carsten@hashicorp.com>
2023-03-13 10:35:37 +0100

    added output

--- main.tf
+++ main.tf
@@ -31,6 +31,10 @@ resource "hcp_consul_cluster_root_token" "token" {
 }
 
 
+output "consul_cluster_id" {
+  value = hcp_consul_cluster.main.cluster_id
+}
+
 output "consul_root_token" {
   value     = hcp_consul_cluster_root_token.token.secret_id
   sensitive = true
